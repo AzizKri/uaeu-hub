9a8b582172331588502465db5450bbabba4f658f
AzizKri <aziz.kanakri@gmail.com>
2024-09-21 23:17:11 +0400

    Create the homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import Search from "./components/search";
+import NavBar from "./components/NavBar";
 import "./styles/index.scss";
+import Question from "./components/Question.tsx";
 
 function App() {
   return (
     <>
-      <div className="navbar">
-        <div className="navbar_logo">UAEU some shit</div>
-        <div className="navbar_buttons">
-          <div className="button selected-button">Home</div>
-          <div className="button">Your Questions</div>
+      <NavBar />
+      <div className="main">
+        <div className="left">
+
         </div>
-        <Search />
+          <div className="middle">
+
+              {/*<div>
+                  <h3>Home</h3>
+
+                  <span>What's your question?</span>
+
+                  <button>Submit</button>
+
+              </div>
+
+              <h3>Recent Questions</h3>
+              <span>20 new questions</span>*/}
+
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+              <Question />
+          </div>
+          <div className="right">
+
+          </div>
       </div>
     </>
   );
