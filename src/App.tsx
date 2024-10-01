77188f15dab2b34f13f7b1edd4ef91c6295abb71
AzizKri <aziz.kanakri@gmail.com>
2024-10-01 13:58:33 +0400

    Refractor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar.tsx";
 import "./styles/index.scss";
-import Question from "./components/Question.tsx";
+import Question from "./components/Question/Question.tsx";
 
 function App() {
   return (
@@ -12,17 +12,15 @@ function App() {
         </div>
           <div className="middle">
 
-              {/*<div>
+              <div>
                   <h3>Home</h3>
 
                   <span>What's your question?</span>
-
                   <button>Submit</button>
-
               </div>
 
               <h3>Recent Questions</h3>
-              <span>20 new questions</span>*/}
+              <span>20 new questions</span>
 
               <Question />
               <Question />
