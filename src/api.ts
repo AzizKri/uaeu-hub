7ea898d39935d504e486e4149d1e70acc242af9d
AzizKri <aziz.kanakri@gmail.com>
2024-10-07 19:29:38 +0400

    Add getLatestPosts function

--- src/api.ts
+++ src/api.ts
@@ -4,3 +4,8 @@ export async function getUserByUsername(username: string) {
     const request = await fetch(base + `/users/get/${username}`)
     return await request.json();
 }
+
+export async function getLatestPosts(amount: number) {
+    const request = await fetch(base + `/posts/get/latest/${amount}`)
+    return await request.json();
+}
