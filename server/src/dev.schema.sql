65adf5e49b33def0d19c841ff567df68580f23b4
AzizKri <aziz.kanakri@gmail.com>
2024-10-08 12:24:27 +0400

    Remove plural S from entities

--- server/src/dev.schema.sql
+++ server/src/dev.schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE comments;
-DROP TABLE posts;
-DROP TABLE users;
+DROP TABLE comment;
+DROP TABLE post;
+DROP TABLE user;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user (
 	username TEXT PRIMARY KEY,
 	displayname TEXT NOT NULL,
 	email TEXT NOT NULL,
@@ -11,36 +11,36 @@ CREATE TABLE IF NOT EXISTS users (
 	pfp TEXT
 );
 
-CREATE TABLE IF NOT EXISTS posts (
+CREATE TABLE IF NOT EXISTS post (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	author TEXT NOT NULL,
 	content TEXT NOT NULL,
 	post_time INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (author) REFERENCES users(username)
+	FOREIGN KEY (author) REFERENCES user(username)
 );
 
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS comment (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	parent_post_id INTEGER NOT NULL,
 	author TEXT NOT NULL,
 	content TEXT NOT NULL,
 	post_time INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (parent_post_id) REFERENCES posts(id),
-	FOREIGN KEY (author) REFERENCES users(username)
+	FOREIGN KEY (parent_post_id) REFERENCES post(id),
+	FOREIGN KEY (author) REFERENCES user(username)
 );
 
 CREATE TABLE IF NOT EXISTS post_likes (
 	post_id INTEGER NOT NULL,
 	user TEXT NOT NULL,
 	PRIMARY KEY (post_id, user),
-	FOREIGN KEY (post_id) REFERENCES posts(id),
-	FOREIGN KEY (user) REFERENCES users(username)
+	FOREIGN KEY (post_id) REFERENCES post(id),
+	FOREIGN KEY (user) REFERENCES user(username)
 );
 
 CREATE TABLE IF NOT EXISTS comment_likes (
 	comment_id INTEGER NOT NULL,
 	username TEXT NOT NULL,
 	PRIMARY KEY (comment_id, username),
-	FOREIGN KEY (comment_id) REFERENCES comments(id),
-	FOREIGN KEY (username) REFERENCES users(username)
+	FOREIGN KEY (comment_id) REFERENCES comment(id),
+	FOREIGN KEY (username) REFERENCES user(username)
 );
