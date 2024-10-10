936385246c80a6f92f84451aaf3e6b402e9ea7f8
AzizKri <aziz.kanakri@gmail.com>
2024-10-10 14:28:02 +0400

    Finish initial post API

--- server/src/v2/routes/post.ts
+++ server/src/v2/routes/post.ts
@@ -1,11 +1,15 @@
 import {Hono} from "hono";
 import {Bindings} from "../../util/types";
+import { createPost, getLatestPosts, getPostByID, getPostsByUser } from '../controllers/post.controller';
 
-const post = new Hono<{ Bindings: Bindings }>();
+const app = new Hono<{ Bindings: Bindings }>();
 
-post.get()
+// spent an hour trying to figure out why /latest/:page? works but /latest doesn't
+// do NOT place any route with parameters above /latest/:page? or the parameterless route will break, with your bones
 
-// user.get('/authenticate', (c) => Authenticate(c));
-// user.get('/:username', (c) => getByUsername(c));
+app.get('/latest/:page?', (c) => getLatestPosts(c))
+app.post('/create', (c) => createPost(c))
+app.get('/user/:username/:page?', (c) => getPostsByUser(c))
+app.get('/:id', (c) => getPostByID(c))
 
-export default post;
+export default app;
