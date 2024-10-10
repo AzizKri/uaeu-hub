345abc87b5d9db04e285548369ea500d829a1afa
AzizKri <aziz.kanakri@gmail.com>
2024-10-10 11:25:25 +0400

    Fix /user/:username endpoint

--- server/src/v2/controllers/user.controller.ts
+++ server/src/v2/controllers/user.controller.ts
@@ -12,20 +12,25 @@ type UserRow = {
 
 export async function Authenticate(c: Context) {
 	// api.uaeu.chat/v2/user/authenticate
+
+	// TODO: Implement authentication
 }
 
 export async function getByUsername(c: Context) {
 	// api.uaeu.chat/v2/user/:username
-	const username: string | null = c.req.param('username');
+	const username = c.req.param('username');
 	const env: Env = c.env;
 
-	if (!username) throw new HTTPException(400, { res: new Response('No username defined', { status: 400 })})
-	await env.DB.prepare(
-		"SELECT * FROM user WHERE username = ?"
-	).bind(username).all<UserRow>().then((result) => {
+	// This is likely impossible but yeah
+	if (username === '') throw new HTTPException(400, { res: new Response('No username defined', { status: 400 })})
+
+	try {
+		const result = await env.DB.prepare(
+			"SELECT * FROM user WHERE username = ?"
+		).bind(username).all<UserRow>();
 		return Response.json(result);
-	}).catch((err) => {
-		console.error(err);
+	} catch (e) {
+		console.log(e);
 		return new Response('Internal Server Error', {status: 500});
-	});
+	}
 }
