691c218a0b93acdb5111b15062556bbf4da6c46e
AzizKri <aziz.kanakri@gmail.com>
2024-10-10 20:00:51 +0400

    CORS, Allow v1, route to attachment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,20 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import user from './v2/routes/user';
 import post from './v2/routes/post';
+import attachment from './v2/routes/attachment';
+import v1 from './v1/index';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use(cors({
+	origin: ['https://uaeu.chat', 'http://localhost:5173']
+}))
+
+app.route('/v1', v1); // deprecated
 app.route('/user', user);
 app.route('/post', post);
+app.route('/attachment', attachment);
 
 app.onError((c, err) => {
 	console.error(c, err);
