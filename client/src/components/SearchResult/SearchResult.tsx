1426c299f8aa6b59517c0da643c0abe8ae4c17cc
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2024-10-14 23:18:19 +0400

    Removed ts-ignore

--- client/src/components/SearchResult/SearchResult.tsx
+++ client/src/components/SearchResult/SearchResult.tsx
@@ -1,6 +1,5 @@
 import styles from './SearchResult.module.scss'
-// @ts-ignore
-export default function SearchResult({ result }) {
+export default function SearchResult({ result } : {result: PostInfo}) {
     return (
         <a href={`/post/${result.id}`}><div className={styles.searchResult}>{result.content}</div></a>
     );
