d8eed3b46b918d7e02e21b24185fb41ace5f0179
AzizKri <aziz.kanakri@gmail.com>
2024-10-14 13:20:59 +0400

    Create searchPosts function

--- server/src/v2/controllers/post.controller.ts
+++ server/src/v2/controllers/post.controller.ts
@@ -2,8 +2,30 @@ import { Context } from 'hono';
 import { HTTPException } from 'hono/http-exception';
 import { v4 as uuidv4 } from 'uuid';
 
+export async function searchPosts(c: Context) {
+	// api.uaeu.chat/post/search/:query
+	const env: Env = c.env;
+	const query = c.req.param('query');
+
+	try {
+		const results = await env.DB.prepare(
+			`SELECT post.*, bm25(posts_fts, 1.0, 0.75) AS rank
+			 FROM post
+							JOIN posts_fts ON post.id = posts_fts.rowid
+			 WHERE posts_fts MATCH ?
+			 ORDER BY rank DESC
+			 LIMIT 10`
+		).bind(query).all<PostRow>();
+
+		return Response.json(results);
+	} catch (e) {
+		console.error(e);
+		return new Response('Internal Server Error', { status: 500 });
+	}
+}
+
 export async function getPostByID(c: Context) {
-	// api.uaeu.chat/v2/post/:id
+	// api.uaeu.chat/post/:id
 	const env: Env = c.env;
 	const id: number = Number(c.req.param('id'));
 
@@ -32,8 +54,42 @@ export async function getPostByID(c: Context) {
 	}
 }
 
+// export async function getBestPosts(c: Context) {
+// 	// api.uaeu.chat/post/latest/:page
+// 	const env: Env = c.env;
+// 	const page = c.req.param('page') ? Number(c.req.param('page')) : 0;
+//
+// 	try {
+// 		const results = await env.DB.prepare(
+// 			`SELECT post.*,
+// 							user.displayname,
+// 							user.pfp,
+// 							COUNT(post_likes.post_id)     AS like_count,
+// 							COUNT(comment.parent_post_id) AS comment_count,
+// 				 (
+// 				 (COUNT(post_likes.post_id) / (SELECT MAX(like_count) FROM post)) * 0.5 +
+// 				 (COUNT(comment.parent_post_id) / (SELECT MAX(comment_count) FROM post)) * 0.35 -
+// 				 (post.post_time / (SELECT MAX(like_count) FROM post)) * 0.15
+// 				 )
+// 			 FROM post
+// 				 LEFT JOIN user
+// 			 ON post.author = user.username
+// 				 LEFT JOIN post_likes on post.id = post_likes.post_id
+// 				 LEFT JOIN comment on post.post_time = comment.post_time
+// 			 GROUP BY post_likes.post_id, post.id
+// 			 ORDER BY post.post_time DESC
+// 			 LIMIT 10 OFFSET (? * 10)`
+// 		).bind(page).all<PostRow>();
+//
+// 		return Response.json(results);
+// 	} catch (e) {
+// 		console.error(e);
+// 		return new Response('Internal Server Error', { status: 500 });
+// 	}
+// }
+
 export async function getLatestPosts(c: Context) {
-	// api.uaeu.chat/v2/post/latest/:page
+	// api.uaeu.chat/post/latest/:page
 	const env: Env = c.env;
 	const page = c.req.param('page') ? Number(c.req.param('page')) : 0;
 
@@ -61,7 +117,7 @@ export async function getLatestPosts(c: Context) {
 }
 
 export async function getPostsByUser(c: Context) {
-	// api.uaeu.chat/v2/post/user/:username/:page?
+	// api.uaeu.chat/post/user/:username/:page?
 	const env: Env = c.env;
 	const { username, page } = c.req.param();
 
@@ -87,7 +143,7 @@ export async function getPostsByUser(c: Context) {
 }
 
 export async function createPost(c: Context) {
-	// api.uaeu.chat/v2/post/create
+	// api.uaeu.chat/post/create
 	const env: Env = c.env;
 	const formData = await c.req.parseBody();
 	const author = formData['author'] as string;
