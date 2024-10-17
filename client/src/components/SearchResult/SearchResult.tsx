54495c713a8fc179cb47123ed8284c841ccc071e
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2024-10-17 09:57:51 +0400

    Small changes to the search results

--- client/src/components/SearchResult/SearchResult.tsx
+++ client/src/components/SearchResult/SearchResult.tsx
@@ -2,7 +2,7 @@ import styles from './SearchResult.module.scss';
 
 export default function SearchResult({ result }: {result: SearchResult}) {
     return (
-        <a href={`/post/${result.id}`}>
+        <a href={`/post/${result.id}`} className={styles.postSearchLink}>
             <div className={styles.searchResult}>{result.content}</div>
         </a>
     );
