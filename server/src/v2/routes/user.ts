fb3ecb9663aaa05a007c382cd71bfa9e7fc499e3
AzizKri <aziz.kanakri@gmail.com>
2024-10-24 00:42:04 +0400

    Implement Login & Signup

--- server/src/v2/routes/user.ts
+++ server/src/v2/routes/user.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
-import { Authenticate, getByUsername } from '../controllers/user.controller';
+import { getByUsername, login, signup } from '../controllers/user.controller';
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/authenticate', (c) => Authenticate(c));
+app.post('/signup', (c) => signup(c));
+app.post('/login', (c) => login(c));
 // app.get('/anon', (c) => generateAnonSessionId(c));
 app.get('/:username', (c) => getByUsername(c));
 
