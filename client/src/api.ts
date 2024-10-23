9e6d1ae1c4c3d06c4fbb82722fda0cd79cc70bf1
AzizKri <aziz.kanakri@gmail.com>
2024-10-24 00:50:25 +0400

    oops

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,5 +1,5 @@
-// const base = 'https://api.uaeu.chat';
-const base = 'http://127.0.0.1:8787';
+const base = 'https://api.uaeu.chat';
+// const base = 'http://127.0.0.1:8787';
 const cdn = 'https://cdn.uaeu.chat';
 
 // api.uaeu.chat/user/anon
