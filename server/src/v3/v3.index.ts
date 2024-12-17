625d5ec04df69d4fa0ba9b810ce2f7ddd3b3322a
AzizKri <skilledon@hotmail.com>
2024-12-17 17:32:10 +0400

    Create V3 Index

---
+++ server/src/v3/v3.index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono';
+import post from './routes/post';
+import user from './routes/user';
+import attachment from './routes/attachment';
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.route('/user', user)
+app.route('/post', post)
+// app.route('/comment', comment)
+app.route('/attachment', attachment)
+
+export default app
