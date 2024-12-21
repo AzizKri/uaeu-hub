7eac7ee87979edc03b9c2b9ec66e3d8fcae5c6c6
AzizKri <aziz.kanakri@gmail.com>
2024-12-21 15:57:31 +0400

    Register user if SK is found without a user

--- server/src/v3/util/util.ts
+++ server/src/v3/util/util.ts
@@ -1,14 +1,29 @@
 import { Context } from 'hono';
 import { hashSessionKey } from './crypto';
+import { anonSignup } from '../controllers/user.controller';
 
 export async function getUserFromSessionKey(c: Context, sessionKey: string) {
     const env: Env = c.env;
     const hashedKey = await hashSessionKey(sessionKey);
 
-    const userResult = await env.DB.prepare(
-        `SELECT user_id FROM session WHERE id = ?`
-    ).bind(hashedKey).first<SessionRow>();
+    try {
+        let userResult = await env.DB.prepare(
+            `SELECT user_id FROM session WHERE id = ?`
+        ).bind(hashedKey).first<SessionRow>();
 
-    if (!userResult) return c.text('User not found', { status: 404 });
-    return userResult.user_id;
+        if (!userResult) {
+            await anonSignup(c);
+
+            const newHashedKey = await hashSessionKey(c.get('sessionKey'));
+
+            userResult = await env.DB.prepare(
+                `SELECT user_id FROM session WHERE id = ?`
+            ).bind(newHashedKey).first<SessionRow>();
+
+            return userResult?.user_id;
+        }
+    } catch (e) {
+        console.log(e);
+        return null;
+    }
 }
