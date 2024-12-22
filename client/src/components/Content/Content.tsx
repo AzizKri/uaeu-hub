534232a9c8d3462b209f9f39abf57d41d60a1cd7
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2024-12-22 16:09:00 +0400

    separate the content into a new component

---
+++ client/src/components/Content/Content.tsx
@@ -0,0 +1,68 @@
+import styles from "./Content.module.scss"
+import LoadingImage from "../LoadingImage/LoadingImage.tsx";
+import {useEffect, useState} from "react";
+import {getAttachmentDetails} from "../../api.ts";
+
+export default function Content({id, content, filename, type}: {id: number, content: string, filename: string | undefined, type: string}) {
+    const [showContent, setShowContent] = useState<boolean>(content.length < 300);
+    const [imageSrc, setImageSrc] = useState<string>("");
+    const [imageDims, setImageDims] = useState<{ width: number, height: number }>({width: 0, height: 0});
+    const [error, setError] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(filename != null);
+
+    useEffect(() => {
+        if (filename) {
+            getAttachmentDetails(filename).then((res) => {
+                if (res.status === 200) {
+                    const fileType = res.data?.type;
+                    if (fileType?.startsWith('image')) {
+                        setImageDims({width: res.data?.metadata.width, height: res.data?.metadata.height});
+                        setImageSrc(`https://cdn.uaeu.chat/attachments/${filename}`);
+                        setError(false);
+                    } else {
+                        setImageSrc(`https://cdn.uaeu.chat/attachments/${filename}`);
+                        setError(false);
+                    }
+                } else {
+                    setError(true);
+                    setIsLoading(false);
+                }
+            }).catch(() => {
+                setError(true);
+                setIsLoading(false);
+            });
+        }
+    }, [filename]);
+
+    return (
+        <>
+        <div className={styles.text}>
+            {type === "post-page" ?
+                (showContent || (type === "post-page") ? content : <> {content.slice(0, 200)} <span>&#8230;</span> </>)
+                :
+                <a href={`/post/${id}`}>
+                    {showContent || (type === "post-page") ? content : <> {content.slice(0, 200)} <span>&#8230;</span> </>}
+                </a>
+            }
+            {showContent ? '' :
+                <span className={styles.show_more} onClick={() => setShowContent(true)}>show more</span>}
+        </div>
+    {/*<ReadOnlyEditor content={editorContent} />*/}
+    {isLoading && !error && <LoadingImage />}
+    {filename != null && !error &&
+    <div className={styles.image} style={{display: isLoading ? 'none' : 'block', height: imageDims.height, width: imageDims.width}}>
+        <img
+            src={imageSrc}
+            alt="post attachment"
+            onLoad={() => setIsLoading(false)}
+            onError={() => {
+                setError(true);
+                setIsLoading(false);
+            }}
+        />
+    </div>
+    }
+    {error && <p>Error Loading the image</p>}
+        </>
+    )
+}
