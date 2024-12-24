122b42816ba5e138fb621c2b7b67e1b2c84744f5
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2024-12-24 11:09:09 +0400

    change the type of post to include top comment info

--- client/src/lib/UpdatePostProvider.tsx
+++ client/src/lib/UpdatePostProvider.tsx
@@ -15,7 +15,7 @@ export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
         setLoading(true);
         try {
             const res = await getLatestPosts(page);
-            console.log(page);
+            console.log(res);
             if (res.results.length == 0) {
                 setNoMorePosts(true);
                 return;
@@ -23,19 +23,40 @@ export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
             setPage((prevPage) => prevPage + 1);
             const fetchedPosts: React.ReactElement[] = []
             for (const post of res.results) {
+                const postInfo: PostInfo = {
+                    id: post.id,
+                    content: post.content,
+                    authorUsername: post.author,
+                    authorDisplayName: post.displayname,
+                    pfp: post.pfp,
+                    postDate: new Date(post.post_time),
+                    filename: post.attachment,
+                    like_count: post.like_count,
+                    comment_count: post.comment_count,
+                    type: "post",
+                    liked: post.like,
+                };
+                const topCommentInfo: CommentInfo = JSON.parse(post.comment);
+                // const topCommentInfo: CommentInfo = {
+                //     attachment: post.top_comment_attachment,
+                //     author: post.top_comment_author,
+                //     author_id: post.top_comment_author_id,
+                //     content: post.top_comment_content,
+                //     display_name: post.top_comment_display_name,
+                //     id: post.top_comment_id,
+                //     level: post.top_comment_level,
+                //     like_count: post.top_comment_like_count,
+                //     liked: post.top_comment_liked,
+                //     parent_post_id: post.top_comment_parent_post_id,
+                //     parent_type: post.top_comment_parent_type,
+                //     pfp: post.top_comment_pfp,
+                //     post_time: post.top_comment_post_time,
+                // }
                 fetchedPosts.push(
                     <Post
                         key={post.id}
-                        id={post.id}
-                        content={post.content}
-                        authorUsername={post.author}
-                        authorDisplayName={post.displayname}
-                        pfp={post.pfp}
-                        postDate={new Date(post.post_time)}
-                        filename={post.attachment}
-                        likes={post.like_count}
-                        comments_count={post.comment_count}
-                        type={"post"}
+                        post_info={postInfo}
+                        top_comment_info={topCommentInfo}
                     />
                 );
             }
