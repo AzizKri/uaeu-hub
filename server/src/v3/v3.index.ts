eec9dbaccb9af56030c9b97793121f03c030f341
AzizKri <aziz.kanakri@gmail.com>
2025-01-01 16:13:01 +0400

    Add new APIs to index

--- server/src/v3/v3.index.ts
+++ server/src/v3/v3.index.ts
@@ -4,13 +4,17 @@ import user from './routes/user';
 import attachment from './routes/attachment';
 import comment from './routes/comment';
 import community from './routes/community';
+import subcomment from './routes/subcomment';
+import tags from './routes/tags';
 
 const app = new Hono<{ Bindings: Env }>();
 
 app.route('/user', user)
 app.route('/post', post)
 app.route('/comment', comment)
+app.route('/subcomment', subcomment)
 app.route('/attachment', attachment)
 app.route('/community', community)
+app.route('/tags', tags)
 
 export default app
