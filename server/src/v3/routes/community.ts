54831e5a98240c8478644c8673a9e9cdef0a98d2
AzizKri <aziz.kanakri@gmail.com>
2025-01-01 16:14:52 +0400

    Create CommunityMembership endpoints + finalize

--- server/src/v3/routes/community.ts
+++ server/src/v3/routes/community.ts
@@ -2,13 +2,19 @@ import { Hono } from 'hono';
 import { JwtVariables } from 'hono/jwt';
 import { authMiddleware } from '../util/middleware';
 import {
-    createCommunity, getCommunitiesSortByActivity,
+    addMemberToCommunity,
+    createCommunity,
+    deleteCommunity,
+    editCommunity,
+    getCommunitiesSortByActivity,
     getCommunitiesSortByCreation,
     getCommunitiesSortByMembers,
-    getCommunity
+    getCommunityById,
+    getCommunityByName, getCommunityMembers, joinCommunity, leaveCommunity,
+    removeMemberFromCommunity
 } from '../controllers/community.controller';
 import { validator } from 'hono/validator';
-import { communitySchema } from '../util/validationSchemas';
+import { communityEditingSchema, communitySchema } from '../util/validationSchemas';
 
 type Variables = JwtVariables
 
@@ -16,28 +22,52 @@ const app = new Hono<{ Bindings: Env, Variables: Variables }>();
 
 app.use('/', authMiddleware);
 
+// Create Community
 app.post('/',
     validator('form', (value, c) => {
         const parsed = communitySchema.safeParse(value);
         if (!parsed.success) {
-            // if false positive, check the tags rule
             return c.text('Invalid community data', 400);
         }
         return parsed.data;
     }),
     (c) => createCommunity(c));
-app.get('/getCommunities', async (c) => {
+
+// Community Memberships
+app.post('/join/:id', (c) => joinCommunity(c));
+app.post('/leave/:id', (c) => leaveCommunity(c));
+app.get('/getMembers/:id', (c) => getCommunityMembers(c));
+app.post('/addMember/:id/:userId', (c) => addMemberToCommunity(c));
+app.delete('/removeMember/:id/:userId', (c) => removeMemberFromCommunity(c));
+
+// Get Communities
+app.get('/getCommunities', (c) => {
     const { sortBy } = c.req.query();
     switch (sortBy) {
         case 'latest':
-            return await getCommunitiesSortByCreation(c);
+            return getCommunitiesSortByCreation(c);
         case 'activity':
-            return await getCommunitiesSortByActivity(c);
+            return getCommunitiesSortByActivity(c);
         case 'members':
         default:
-            return await getCommunitiesSortByMembers(c);
+            return getCommunitiesSortByMembers(c);
     }
-})
-app.get('/', (c) => getCommunity(c));
+});
+app.get('/:id', (c) => getCommunityById(c));
+app.get('/:name', (c) => getCommunityByName(c));
+
+// Edit Community
+app.post('/:id',
+    validator('form', (value, c) => {
+        const parsed = communityEditingSchema.safeParse(value);
+        if (!parsed.success) {
+            // if false positive, check the tags rule
+            return c.text('Invalid community data', 400);
+        }
+        return parsed.data;
+    }), (c) => editCommunity(c));
+
+// Delete Community
+app.delete('/:id', (c) => deleteCommunity(c));
 
 export default app;
