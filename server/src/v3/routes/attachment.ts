3bd920ac68c8dbb9f29b5ccf2bd68f692ebb7792
AzizKri <aziz.kanakri@gmail.com>
2025-01-01 16:15:44 +0400

    very important commit

--- server/src/v3/routes/attachment.ts
+++ server/src/v3/routes/attachment.ts
@@ -13,6 +13,7 @@ const app = new Hono<{ Bindings: Env, Variables: Variables }>();
 
 app.use('/', authMiddleware);
 app.use('/', uploadAttachmentLimitMiddleware);
+
 app.post('/', (c) => uploadAttachment(c), bodyLimit({						// api.uaeu.chat/attachment
     maxSize: 10 * 1024 * 1024,
     onError: (c) => c.text('File too large', 400)
