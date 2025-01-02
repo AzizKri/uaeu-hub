811f87a425680db1b202efe55e549ab6959217a7
AzizKri <aziz.kanakri@gmail.com>
2025-01-02 14:38:21 +0400

    Create Get Latest/Best posts from my communities

--- server/src/v3/routes/post.ts
+++ server/src/v3/routes/post.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono';
 import {
-    createPost, deletePost, getBestPosts,
+    createPost,
+    deletePost,
+    getBestPosts, getBestPostsFromMyCommunities,
     getLatestPosts,
+    getLatestPostsFromMyCommunities,
     getPostByID,
-    getPostsByUser, likePost,
+    getPostsByUser,
+    likePost,
     searchPosts
 } from '../controllers/post.controller';
 import type { JwtVariables } from 'hono/jwt';
@@ -21,10 +25,12 @@ app.use('/', authMiddleware, postRateLimitMiddleware);
 app.post('/', (c) => createPost(c));                                // api.uaeu.chat/post
 app.delete('/:id', (c) => deletePost(c));                           // api.uaeu.chat/post/:id
 app.post('/like/:id', (c) => likePost(c));				            // api.uaeu.chat/post/like/:id
-app.get('/latest', (c) => getLatestPosts(c));				// api.uaeu.chat/post/latest/:page
-app.get('/best', (c) => getBestPosts(c));				    // api.uaeu.chat/post/best/:page
-app.get('/search', (c) => searchPosts(c));					// api.uaeu.chat/post/search/:query
-app.get('/user/:user', (c) => getPostsByUser(c));		    // api.uaeu.chat/post/user/:username/:page
+app.get('/latest', (c) => getLatestPosts(c));				        // api.uaeu.chat/post/latest?page=
+app.get('/best', (c) => getBestPosts(c));				            // api.uaeu.chat/post/best?page=
+app.get('/myLatest', (c) => getLatestPostsFromMyCommunities(c));	// api.uaeu.chat/post/myLatest?page=
+app.get('/myBest', (c) => getBestPostsFromMyCommunities(c));	    // api.uaeu.chat/post/myBest?page=
+app.get('/search', (c) => searchPosts(c));					        // api.uaeu.chat/post/search?query=
+app.get('/user/:user', (c) => getPostsByUser(c));		            // api.uaeu.chat/post/user/:username?page=
 app.get('/:id', (c) => getPostByID(c));								// api.uaeu.chat/post/:id
 
 export default app;
