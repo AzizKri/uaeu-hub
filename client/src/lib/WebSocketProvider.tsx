6a7d137907d456961a9c5734131b6e83a1dd45f2
AzizKri <aziz.kanakri@gmail.com>
2025-01-03 23:48:10 +0400

    Create WebSocket Provider & Context

---
+++ client/src/lib/WebSocketProvider.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { createWebsocketConnection } from '../api.ts';
+import { WebSocketContext } from './context.ts';
+
+export function WebSocketProvider({ children }: { children: ReactNode }) {
+    const [ws, setWs] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        createWebsocketConnection().then(
+            (websocket) => {
+                if (!websocket) {
+                    console.log('Failed to create WebSocket connection');
+                    return;
+                }
+                setWs(ws);
+
+                websocket.onopen = () => console.log('Connection established');
+                websocket.onclose = () => console.log('Connection closed');
+                websocket.onerror = (error) => console.log('WebSocket error:', error);
+
+                return () => websocket.close();
+            },
+            (error) => {
+                console.error('Failed to create WebSocket connection:', error);
+            }
+        )
+    }, [ws]);
+
+    return <WebSocketContext.Provider value={ws}>{children}</WebSocketContext.Provider>;
+}
--- client/src/lib/context.ts
+++ client/src/lib/context.ts
@@ -2,3 +2,4 @@ import {createContext} from "react";
 
 export const UpdatePostsContext = createContext<UpdatePostsContextInterface | null>(null);
 export const UserContext = createContext<UserContextInterface | null>(null);
+export const WebSocketContext = createContext<WebSocket | null>(null);
