3f79e384bb8b576edf45fcd7b550488c06731d07
AzizKri <aziz.kanakri@gmail.com>
2025-01-04 00:11:53 +0400

    Use WebSocketInterface in Context

--- client/src/lib/context.ts
+++ client/src/lib/context.ts
@@ -2,4 +2,4 @@ import {createContext} from "react";
 
 export const UpdatePostsContext = createContext<UpdatePostsContextInterface | null>(null);
 export const UserContext = createContext<UserContextInterface | null>(null);
-export const WebSocketContext = createContext<WebSocket | null>(null);
+export const WebSocketContext = createContext<WebSocketInterface | null>(null);
