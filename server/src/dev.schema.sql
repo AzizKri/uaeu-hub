e26a56b8f14f2c08ccee375ef89fae548569c5f7
AzizKri <aziz.kanakri@gmail.com>
2025-01-03 23:40:05 +0400

    Create Notification & Websocket tables

--- server/src/dev.schema.sql
+++ server/src/dev.schema.sql
@@ -45,7 +45,9 @@ CREATE TABLE IF NOT EXISTS session
 (
     id         TEXT PRIMARY KEY,
     user_id    INTEGER NOT NULL,
+    salt       TEXT    NOT NULL,
     created_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ip         TEXT,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
 );
 
@@ -278,11 +280,56 @@ CREATE TABLE IF NOT EXISTS subcomment_like
 (
     subcomment_id INTEGER NOT NULL,
     user_id       TEXT    NOT NULL,
-    created_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at    INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (subcomment_id, user_id),
     FOREIGN KEY (subcomment_id) REFERENCES subcomment (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
 );
+
+/* Websocket Table */
+
+CREATE TABLE IF NOT EXISTS websocket
+(
+    user_id    INTEGER NOT NULL,
+    socket_id  TEXT    NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    used       BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (user_id, socket_id),
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+);
+
+/* Notification Table */
+
+CREATE TABLE IF NOT EXISTS notification
+(
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    recipient_id INTEGER NOT NULL,
+    sender_id    INTEGER,
+    action       TEXT    NOT NULL, /* 'like', 'comment', 'mention', 'follow' */
+    entity_id    INTEGER, /* Post ID, Comment ID, etc. */
+    entity_type  TEXT    NOT NULL, /* 'post', 'comment', 'subcomment', 'user' */
+    read         BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at   INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipient_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (sender_id) REFERENCES user (id) ON DELETE CASCADE
+);
+
+CREATE VIEW IF NOT EXISTS notification_view AS
+SELECT notification.id,
+       notification.recipient_id,
+       recipient.username    AS recipient,
+       recipient.displayname AS recipient_displayname,
+       notification.sender_id,
+       sender.username       AS sender,
+       sender.displayname    AS sender_displayname,
+       notification.type,
+       notification.entity_id,
+       notification.entity_type,
+       notification.read,
+       notification.created_at
+FROM notification
+         JOIN user AS recipient ON notification.recipient_id = recipient.id
+         LEFT JOIN user AS sender ON notification.sender_id = sender.id;
 
 /* Full Text Search Table */
 
