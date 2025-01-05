c4d820bc7688830ee50f441cc677802dc2d96243
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-05 21:58:55 +0400

    fix: size of loader image to fit in different places

--- client/src/components/Reusable/LoadingImage/LoadingImage.tsx
+++ client/src/components/Reusable/LoadingImage/LoadingImage.tsx
@@ -1,9 +1,9 @@
 import styles from "./LoadingImage.module.scss";
 
-export default function LoadingImage() {
+export default function LoadingImage({width}: {width: string}) {
     return (
         <div className={styles.container}>
-            <div className={styles.loader}></div>
+            <div className={styles.loader} style={{width: width, height: width}}></div>
         </div>
     )
 }
