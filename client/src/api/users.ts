6f837a7c0bebaf4d01132d74dc50995f9d95279f
AzizKri <aziz.kanakri@gmail.com>
2025-01-16 11:50:53 +0400

    Implement getUserCommunities

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -6,3 +6,7 @@ export async function getUserByUsername(username: string) {
     return { status: request.status, data: await request.json() };
 }
 
+export async function getUserCommunities(userId: number) {
+    const request = await fetch(base + `/user/${userId}/communities`, { credentials: 'include' });
+    return { status: request.status, data: await request.json() };
+}
