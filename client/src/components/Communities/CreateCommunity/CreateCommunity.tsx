18404b16c400142a6ce9568a0927f200e246cab6
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-19 08:20:16 +0400

    fix: use the image uploader component in create community and user profile

--- client/src/components/Communities/CreateCommunity/CreateCommunity.tsx
+++ client/src/components/Communities/CreateCommunity/CreateCommunity.tsx
@@ -7,10 +7,6 @@ import React, {
     useRef,
     useState,
 } from "react";
-import { deleteAttachment, uploadAttachment } from "../../../api/attachmets.ts";
-import communityIcon from "../../../assets/community-icon.jpg";
-import editImage from "../../../assets/image-edit-outline.svg";
-import loadingGiv from "../../../assets/loading_gif.gif";
 import {
     communityExists,
     createCommunity,
@@ -22,13 +18,7 @@ import plusIcon from "../../../assets/plus.svg";
 import { useNavigate } from "react-router-dom";
 import LoaderDots from "../../Reusable/LoaderDots/LoaderDots.tsx";
 import { debounce } from "../../../lib/utils/tools.ts";
-
-interface UploadState {
-    status: "IDLE" | "UPLOADING" | "COMPLETED" | "ERROR";
-    fileName?: string;
-    file: File | null;
-    preview: string | ArrayBuffer | null;
-}
+import ImageUploader, {ImageUploaderMethods} from "../../Reusable/ImageUploader/ImageUploader.tsx";
 
 interface props {
     type: "CREATE" | "EDIT";
@@ -70,7 +60,6 @@ export default function CreateCommunity({
                   preview: null,
               },
     );
-    const imageInputRef = useRef<HTMLInputElement>(null);
     const [selectedTags, setSelectedTags] = useState<
         { id: number; name: string }[]
     >([]);
@@ -85,6 +74,7 @@ export default function CreateCommunity({
     const [checkingName, setCheckingName] = useState<boolean>(false);
     const [nameFocus, setNameFocus] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string>("");
+    const childRef = useRef<ImageUploaderMethods>(null);
 
     useEffect(() => {
         getTags().then((res) => {
@@ -105,78 +95,6 @@ export default function CreateCommunity({
         });
     }, [tags]);
 
-    const handleFileUpload = async (
-        event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-        const selectedFile = event.target.files?.[0];
-        if (!selectedFile) return;
-
-        try {
-            setUploadState({
-                status: "UPLOADING",
-                file: selectedFile,
-                preview: null,
-            });
-
-            // Create preview
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                setUploadState((prev) => ({
-                    ...prev,
-                    preview: e.target?.result || null,
-                }));
-            };
-            reader.readAsDataURL(selectedFile);
-
-            // Upload file
-            const response = await uploadAttachment([selectedFile]);
-
-            if (response.status === 201) {
-                setUploadState((prev) => ({
-                    ...prev,
-                    status: "COMPLETED",
-                    fileName: response.filename,
-                }));
-            } else {
-                console.error(`Upload failed with status: ${response.status}`);
-                setUploadState({
-                    status: "ERROR",
-                    file: null,
-                    preview: null,
-                });
-            }
-        } catch (error) {
-            console.error("File upload error:", error);
-            setUploadState({
-                status: "ERROR",
-                file: null,
-                preview: null,
-            });
-        }
-    };
-
-    const removeImage = () => {
-        if (typeof uploadState.fileName === "string")
-            deleteAttachment(uploadState.fileName).then(() =>
-                console.log("attachment deleted"),
-            );
-        setUploadState({
-            status: "IDLE",
-            file: null,
-            preview: null,
-        });
-        if (imageInputRef.current) {
-            imageInputRef.current.value = "";
-        }
-    };
-
-    const handleChangeImage = () => {
-        if (typeof uploadState.fileName === "string")
-            deleteAttachment(uploadState.fileName).then(() =>
-                console.log("attachment deleted"),
-            );
-        imageInputRef.current?.click();
-    };
 
     const handleFormSubmit: React.FormEventHandler = (e) => {
         e.preventDefault();
@@ -247,7 +165,9 @@ export default function CreateCommunity({
     );
 
     const handleCancel = () => {
-        removeImage();
+        if (childRef.current) {
+            childRef.current.removeImage();
+        }
         onClose();
     };
 
@@ -309,57 +229,11 @@ export default function CreateCommunity({
     return (
         <Modal onClose={onClose}>
             <div className={styles.container}>
-                <div className={styles.imagePreview}>
-                    <img
-                        src={
-                            uploadState.status === "COMPLETED" &&
-                            typeof uploadState.preview === "string"
-                                ? uploadState.preview
-                                : uploadState.status === "IDLE" &&
-                                    typeof uploadState.preview === "string"
-                                  ? uploadState.preview
-                                  : uploadState.status === "UPLOADING"
-                                    ? loadingGiv
-                                    : communityIcon
-                        }
-                        alt="uploaded image preview"
-                    />
-                    <div
-                        className={styles.changeImage}
-                        onClick={handleChangeImage}
-                    >
-                        <img src={editImage} alt="edit image" />
-                    </div>
-                    {uploadState.status === "COMPLETED" && (
-                        <div
-                            className={styles.cancelImage}
-                            onClick={removeImage}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                width="20"
-                                height="20"
-                            >
-                                <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                            </svg>
-                        </div>
-                    )}
-                </div>
+                <ImageUploader type="COMMUNITY" setUploadState={setUploadState} uploadState={uploadState} ref={childRef}/>
                 {errorMessage && (
                     <div className={styles.errorMessage}>{errorMessage}</div>
                 )}
                 <form className={styles.form} onSubmit={handleFormSubmit}>
-                    <input
-                        id="image-upload"
-                        ref={imageInputRef}
-                        className={styles.imageInput}
-                        type="file"
-                        accept="image/*"
-                        onChange={handleFileUpload}
-                        hidden
-                    />
                     <label
                         htmlFor="name-input"
                         className={styles.label}
--- client/src/components/UserProfilePage/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfilePage/UserProfile/UserProfile.tsx
@@ -1,10 +1,9 @@
-import React, {useEffect, useRef, useState} from 'react';
+import {useEffect, useState} from 'react';
 import styles from './UserProfile.module.scss';
 import {Outlet, useLocation, useNavigate, useParams} from "react-router-dom";
 import { getUserByUsername} from '../../../api/users.ts';
-import editImage from "../../../assets/image-edit-outline.svg";
-import {deleteAttachment, uploadAttachment} from "../../../api/attachmets.ts";
 import {useUser} from "../../../lib/utils/hooks.ts";
+import ImageUploader from "../../Reusable/ImageUploader/ImageUploader.tsx";
 
 const authTabs = [
     { label: 'Posts' },
@@ -16,13 +15,6 @@ const tabs = [
     { label: 'Posts' },
     { label: 'Communities' },
 ];
-
-interface UploadState {
-    status: "IDLE" | "UPLOADING" | "COMPLETED" | "ERROR";
-    fileName?: string;
-    file: File | null;
-    preview: string | ArrayBuffer | null;
-}
 
 export default function UserProfile () {
     // State for the current tab
@@ -39,7 +31,7 @@ export default function UserProfile () {
                 file: null,
                 preview: null,}
     );
-    const imageInputRef = useRef<HTMLInputElement>(null);
+
 
     useEffect(() => {
         // const checkAuth = async (username : string) => {
@@ -92,87 +84,12 @@ export default function UserProfile () {
             }});
     };
 
-    const handleFileUpload = async (
-        event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-        const selectedFile = event.target.files?.[0];
-        if (!selectedFile) return;
-
-        try {
-            setUploadState({
-                status: "UPLOADING",
-                file: selectedFile,
-                preview: null,
-            });
-
-            // Create preview
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                setUploadState((prev) => ({
-                    ...prev,
-                    preview: e.target?.result || null,
-                }));
-            };
-            reader.readAsDataURL(selectedFile);
-
-            // Upload file
-            const response = await uploadAttachment([selectedFile]);
-
-            if (response.status === 201) {
-                setUploadState((prev) => ({
-                    ...prev,
-                    status: "COMPLETED",
-                    fileName: response.filename,
-                }));
-            } else {
-                console.error(`Upload failed with status: ${response.status}`);
-                setUploadState({
-                    status: "ERROR",
-                    file: null,
-                    preview: null,
-                });
-            }
-        } catch (error) {
-            console.error("File upload error:", error);
-            setUploadState({
-                status: "ERROR",
-                file: null,
-                preview: null,
-            });
-        }
-    };
-
-    const handleChangeImage = () => {
-        if (typeof uploadState.fileName === "string")
-            deleteAttachment(uploadState.fileName).then(() =>
-                console.log("attachment deleted"),
-            );
-        imageInputRef.current?.click();
-    };
-
     return (
         <div className={styles.userProfileContainer}>
             <div className={styles.userProfileHeader}>
                 <div className={styles.userHeader}>
                     <div className={styles.userAvatar}>
-                        <div className={styles.avatarIcon}
-                             style={{backgroundImage: `url(${profileUser ? (profileUser.pfp ? profileUser.pfp : '') : ''})`}}>
-                            {isAuthorized && <div
-                                className={styles.changeImage}
-                                onClick={handleChangeImage}
-                            >
-                                <img src={editImage} alt="edit image"/>
-                                <input
-                                    id="image-upload"
-                                    ref={imageInputRef}
-                                    className={styles.imageInput}
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={handleFileUpload}
-                                    hidden
-                                />
-                            </div>}
-                        </div>
+                        <ImageUploader uploadState={uploadState} setUploadState={setUploadState} type="PROFILE" />
                     </div>
                     <div className={styles.userInfo}>
                         <h1 className={styles.displayName}>{profileUser ? profileUser.displayName : ''}</h1>
