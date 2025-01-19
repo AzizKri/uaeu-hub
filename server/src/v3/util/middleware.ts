5ba133dc0a4550317361caf4e7bb0c00d37bdd53
AzizKri <aziz.kanakri@gmail.com>
2025-01-19 16:27:59 +0400

    Create textContentModerationMiddleware

--- server/src/v3/util/middleware.ts
+++ server/src/v3/util/middleware.ts
@@ -140,3 +140,27 @@ export const uploadAttachmentLimitMiddleware = createMiddleware(
         await next();
     }
 );
+
+export const textContentModerationMiddleware = createMiddleware(
+    async (c, next) => {
+        let content;
+
+        // Parser FormBody
+        if (c.req.header('Content-Type')?.includes('multipart/form-data') || c.req.header('Content-Type')?.includes('application/x-www-form-urlencoded')) {
+            const formData = await c.req.parseBody();
+            content = formData['content'] as string;
+        } else {
+            return c.json({ message: 'Unsupported content type', status: 400 }, 400);
+        }
+
+        // No content?
+        if (!content) return c.json({ message: 'No content provided', status: 400 }, 400);
+
+        // 1984
+        // TODO Currently unimplemented, thinking whether we want to do this or not
+
+        c.set('moderatedContent', content);
+
+        await next();
+    }
+);
