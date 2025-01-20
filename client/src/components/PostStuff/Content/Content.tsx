76a4f04127c2efc7707d2722606176cf9d99d452
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-20 21:45:20 +0400

    fix: don't request attachment details on each post load

--- client/src/components/PostStuff/Content/Content.tsx
+++ client/src/components/PostStuff/Content/Content.tsx
@@ -1,47 +1,67 @@
-import styles from "./Content.module.scss"
+import styles from "./Content.module.scss";
 import LineSpinner from "../../Reusable/Animations/LineSpinner/LineSpinner.tsx";
-import React, {useEffect, useState} from "react";
-import {getAttachmentDetails} from "../../../api/attachmets.ts";
+import React, { useState } from "react";
+import {assetsBase} from "../../../api/api.ts";
+// import { getAttachmentDetails } from "../../../api/attachmets.ts";
 
-export default function Content({content, filename, type}: {
-    content: string,
-    filename: string | undefined,
-    type: string,
+export default function Content({
+    content,
+    filename,
+    type,
+}: {
+    content: string;
+    filename: string | undefined;
+    type: string;
 }) {
-    const [showContent, setShowContent] = useState<boolean>(content.length < 300);
-    const [imageSrc, setImageSrc] = useState<string>("");
-    const [imageDims, setImageDims] = useState<{ width: number, height: number }>({width: 0, height: 0});
+
+    const [showContent, setShowContent] = useState<boolean>(
+        content.length < 300,
+    );
+    // const [imageSrc, setImageSrc] = useState<string>("");
+    // const [imageDims, setImageDims] = useState<{
+    //     width: number;
+    //     height: number;
+    // }>({ width: 0, height: 0 });
     const [error, setError] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(filename != null);
 
-    useEffect(() => {
-        if (filename) {
-            getAttachmentDetails(filename).then((res) => {
-                if (res.status === 200) {
-                    const fileType = res.data?.type;
-                    if (fileType?.startsWith('image')) {
-                        setImageDims({width: res.data?.metadata.width, height: res.data?.metadata.height});
-                        setImageSrc(`https://cdn.uaeu.chat/attachments/${filename}`);
-                        setError(false);
-                    } else {
-                        setImageSrc(`https://cdn.uaeu.chat/attachments/${filename}`);
-                        setError(false);
-                    }
-                } else {
-                    setError(true);
-                    setIsLoading(false);
-                }
-            }).catch(() => {
-                setError(true);
-                setIsLoading(false);
-            });
-        }
-    }, [filename]);
+    // useEffect(() => {
+    //     if (filename) {
+    //         getAttachmentDetails(filename)
+    //             .then((res) => {
+    //                 if (res.status === 200) {
+    //                     const fileType = res.data?.type;
+    //                     if (fileType?.startsWith("image")) {
+    //                         setImageDims({
+    //                             width: res.data?.metadata.width,
+    //                             height: res.data?.metadata.height,
+    //                         });
+    //                         setImageSrc(
+    //                             `https://cdn.uaeu.chat/attachments/${filename}`,
+    //                         );
+    //                         setError(false);
+    //                     } else {
+    //                         setImageSrc(
+    //                             `https://cdn.uaeu.chat/attachments/${filename}`,
+    //                         );
+    //                         setError(false);
+    //                     }
+    //                 } else {
+    //                     setError(true);
+    //                     setIsLoading(false);
+    //                 }
+    //             })
+    //             .catch(() => {
+    //                 setError(true);
+    //                 setIsLoading(false);
+    //             });
+    //     }
+    // }, [filename]);
 
     const handleShowMore: React.MouseEventHandler<HTMLSpanElement> = (e) => {
         e.stopPropagation();
-        setShowContent(true)
-    }
+        setShowContent(true);
+    };
 
     return (
         <>
@@ -54,28 +74,35 @@ export default function Content({content, filename, type}: {
                         <span>&#8230;</span>
                     </>
                 )}
-                {!showContent && !(type === "post-page") && <span className={styles.show_more} onClick={handleShowMore}>show more</span>}
+                {!showContent && !(type === "post-page") && (
+                    <span className={styles.show_more} onClick={handleShowMore}>
+                        show more
+                    </span>
+                )}
             </div>
             {/*<ReadOnlyEditor content={editorContent} />*/}
-            {isLoading && !error && <LineSpinner width="100%"/>}
-            {filename != null && !error && <div className={styles.image}
-                                                style={{
-                                                    display: isLoading ? 'none' : 'block',
-                                                    height: imageDims.height,
-                                                    width: imageDims.width
-                                                }}
-            >
-                <img
-                    src={imageSrc}
-                    alt="post attachment"
-                    onLoad={() => setIsLoading(false)}
-                    onError={() => {
-                        setError(true);
-                        setIsLoading(false);
+            {isLoading && !error && <LineSpinner width="100%" />}
+            {filename && !error && (
+                <div
+                    className={styles.image}
+                    style={{
+                        display: isLoading ? "none" : "block",
+                        // height: imageDims.height,
+                        // width: imageDims.width,
                     }}
-                />
-            </div>}
+                >
+                    <img
+                        src={`${assetsBase}/attachments/${filename}`}
+                        alt="post attachment"
+                        onLoad={() => setIsLoading(false)}
+                        onError={() => {
+                            setError(true);
+                            setIsLoading(false);
+                        }}
+                    />
+                </div>
+            )}
             {error && <p>Error Loading the image</p>}
         </>
-    )
+    );
 }
