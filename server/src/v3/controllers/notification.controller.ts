fd92623bb7351232b408f5038ea220a077e99adf
AzizKri <aziz.kanakri@gmail.com>
2025-01-25 01:56:50 +0400

    Create a mark as read endpoint

--- server/src/v3/controllers/notification.controller.ts
+++ server/src/v3/controllers/notification.controller.ts
@@ -24,3 +24,28 @@ export async function getNotifications(c: Context) {
         return c.text('Internal Server Error', { status: 500 });
     }
 }
+
+export async function readNotifications(c: Context) {
+    const env: Env = c.env;
+
+    // Get userId & isAnonymous from Context
+    const userId = c.get('userId') as number;
+    const isAnonymous = c.get('isAnonymous') as boolean;
+
+    // Check if user is valid and not anonymous
+    if (!userId || isAnonymous) return c.text('Unauthorized', { status: 401 });
+
+    try {
+        // Mark all notifications as read
+        await env.DB.prepare(`
+            UPDATE notification
+            SET is_read = 1
+            WHERE recipient_id = ?
+        `).bind(userId).run();
+
+        return c.text('Notifications marked as read', { status: 200 });
+    } catch (e) {
+        console.log(e);
+        return c.text('Internal Server Error', { status: 500 });
+    }
+}
--- server/src/v3/routes/notification.ts
+++ server/src/v3/routes/notification.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, readNotifications } from '../controllers/notification.controller';
 import { authMiddlewareCheckOnly } from '../util/middleware';
 
 
 const app = new Hono<{ Bindings: Env }>();
 
 app.get('/', authMiddlewareCheckOnly, (c) => getNotifications(c));
+app.post('/read', authMiddlewareCheckOnly, (c) => readNotifications(c));
 
 export default app;
