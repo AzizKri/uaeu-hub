89b614a01acf8fff5932cda51b61aa4419093417
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-26 08:34:26 +0400

    fix the problem of duplicated posts

--- client/src/contexts/updatePosts/UpdatePostsProvider.tsx
+++ client/src/contexts/updatePosts/UpdatePostsProvider.tsx
@@ -1,26 +1,25 @@
-import React, {useState} from 'react';
+import React, {createContext, useState} from 'react';
 import {getLatestPosts} from "../../api/posts.ts";
 import Post from "../../components/PostStuff/Post/Post.tsx";
-import {UpdatePostsContext} from "../utils/context.ts";
 
-export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
+export const UpdatePostsContext = createContext<UpdatePostsContextInterface | null>(null);
+
+export const UpdatePostsProvider = ({children}: {children: React.ReactNode}) => {
     const [posts, setPosts] = useState<React.ReactElement[]>([]);
-    const [page, setPage] = useState<number>(0);
+    // const [page, setPage] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(false);
     const [noMorePosts, setNoMorePosts] = useState<boolean>(false);
 
     const updatePosts = async () => {
-        if (loading || noMorePosts) return;
+        if (loading || noMorePosts || window.location.pathname !== "/") return;
 
         setLoading(true);
         try {
-            const res = await getLatestPosts(page);
-            // console.log(res);
+            const res = await getLatestPosts(posts.length);
             if (res.data.length == 0) {
                 setNoMorePosts(true);
                 return;
             }
-            setPage((prevPage) => prevPage + 1);
             const fetchedPosts: React.ReactElement[] = []
             for (const post of res.data) {
                 const postInfo: PostInfo = {
@@ -37,22 +36,6 @@ export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
                     liked: post.liked,
                 };
 
-                // const tc = JSON.parse(post.top_comment);
-                // const topCommentInfo: CommentInfo | undefined = tc ? {
-                //         attachment: tc.attachment,
-                //         author: tc.author,
-                //         authorId: tc.author_id,
-                //         content: tc.content,
-                //         displayName: tc.displayname,
-                //         id: tc.id,
-                //         likeCount: tc.like_count,
-                //         liked: tc.liked,
-                //         parentId: tc.parent_post_id,
-                //         pfp: tc.pfp,
-                //         postTime: new Date(tc.post_time),
-                //         commentCount: tc.comment_count,
-                //     } : undefined;
-                //
                 const communityInfo: CommunityInfoSimple = {
                     name: post.community,
                     icon: post.community_icon
@@ -61,7 +44,6 @@ export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
                     <Post
                         key={post.id}
                         postInfo={postInfo}
-                        // topCommentInfo={topCommentInfo}
                         communityInfo={communityInfo}
                         from=""
                     />
@@ -91,4 +73,3 @@ export const UpdatePostProvider = ({children}: {children: React.ReactNode}) => {
         </UpdatePostsContext.Provider>
     )
 }
-
