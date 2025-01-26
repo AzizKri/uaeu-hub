b28772656ec6290cfab77de911dce33f254ca38e
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-26 09:48:42 +0400

    feat: add a button in the bottom to show more posts from the user

--- client/src/components/UserProfilePage/UserPosts/UserPosts.tsx
+++ client/src/components/UserProfilePage/UserPosts/UserPosts.tsx
@@ -1,13 +1,17 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {getPostsByUser} from "../../../api/posts.ts";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getPostsByUser } from "../../../api/posts.ts";
 import Post from "../../PostStuff/Post/Post.tsx";
-import styles from "../UserContent.module.scss"
+import styles from "../UserContent.module.scss";
+import ThreeDotsLine from "../../Reusable/Animations/ThreeDotsLine/ThreeDotsLine.tsx";
 
-export default function UserPosts () {
+export default function UserPosts() {
     const [userPosts, setUserPosts] = useState<React.ReactElement[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const { username } =  useParams<{ username: string }>();
+    const [isLoadingMorePosts, setIsLoadingMorePosts] =
+        useState<boolean>(false);
+    const [noMoreComments, setNoMoreComments] = useState<boolean>(false);
+    const { username } = useParams<{ username: string }>();
 
     useEffect(() => {
         if (!username) return;
@@ -19,7 +23,7 @@ export default function UserPosts () {
                     setIsLoading(false);
                     return;
                 }
-                const fetchedPosts: React.ReactElement[] = []
+                const fetchedPosts: React.ReactElement[] = [];
                 for (const post of res.data) {
                     const postInfo: PostInfo = {
                         id: post.id,
@@ -36,15 +40,15 @@ export default function UserPosts () {
                     };
                     const communityInfo: CommunityInfoSimple = {
                         name: post.community,
-                        icon: post.community_icon
-                    }
+                        icon: post.community_icon,
+                    };
                     fetchedPosts.push(
                         <Post
                             key={post.id}
                             postInfo={postInfo}
                             communityInfo={communityInfo}
                             from={`user/${username}`}
-                        />
+                        />,
                     );
                 }
                 setUserPosts((prevPosts) => [...fetchedPosts, ...prevPosts]);
@@ -56,11 +60,72 @@ export default function UserPosts () {
         }
     }, [username]);
 
+    const handleShowMore = async () => {
+        setIsLoadingMorePosts(true);
+
+        getPostsByUser(username!, userPosts.length)
+            .then((res) => {
+                console.log("getting more user posts");
+                if (res.data.length < 10) {
+                    setNoMoreComments(true);
+                }
+                setUserPosts((prev) => [
+                    ...prev,
+                    ...res.data.map(
+                        (post: {
+                            id: number;
+                            content: string;
+                            author: string;
+                            displayname: string;
+                            pfp: string;
+                            post_time: string | number | Date;
+                            attachment: string;
+                            like_count: number;
+                            comment_count: number;
+                            like: boolean;
+                            community: string;
+                            community_icon: string;
+                        }) => (
+                            <Post
+                                key={post.id}
+                                postInfo={{
+                                    id: post.id,
+                                    content: post.content,
+                                    authorUsername: post.author,
+                                    authorDisplayName: post.displayname,
+                                    pfp: post.pfp,
+                                    postDate: new Date(post.post_time),
+                                    filename: post.attachment,
+                                    likeCount: post.like_count,
+                                    commentCount: post.comment_count,
+                                    type: "POST",
+                                    liked: post.like,
+                                }}
+                                communityInfo={{
+                                    name: post.community,
+                                    icon: post.community_icon,
+                                }}
+                                from={`user/${username}`}
+                            />
+                        ),
+                    ),
+                ]);
+            })
+            .finally(() => {
+                setIsLoadingMorePosts(false);
+            });
+    };
+
     return isLoading ? (
         <span>Loading...</span>
     ) : (
-        <div className={styles.userContentContainer}>
-            {userPosts}
-        </div>
-    )
+        <>
+            <div className={styles.userContentContainer}>{userPosts}</div>
+            {userPosts.length && !noMoreComments && (
+                <button className={styles.show_more} onClick={handleShowMore}>
+                    {isLoadingMorePosts ? <ThreeDotsLine /> : "Show More"}
+                </button>
+            )}
+        </>
+    );
 }
