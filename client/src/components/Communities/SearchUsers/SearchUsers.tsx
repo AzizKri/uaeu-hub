dc7bff32e755a7b6e5deab9f10d4d5d5cedb07c6
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-26 08:26:14 +0400

    fix: component to search for users

---
+++ client/src/components/Communities/SearchUsers/SearchUsers.tsx
@@ -0,0 +1,98 @@
+import styles from "./SearchUsers.module.scss";
+import React, { useCallback, useState } from "react";
+// import {useNavigate} from "react-router-dom";
+import { debounce } from "../../../utils/tools.ts";
+import { searchUser } from "../../../api/users.ts";
+import searchIcon from "../../../assets/search.svg";
+import LineSpinner from "../../Reusable/Animations/LineSpinner/LineSpinner.tsx";
+import UserPreview from "../../UserPreview/UserPreview.tsx";
+
+export default function SearchUsers({ communityId }: { communityId: number }) {
+    const [results, setResults] = useState<UserInfo[]>([]);
+    const [searchValue, setSearchValue] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const getResults = useCallback(
+        debounce(async (searchValue: string) => {
+            searchUser(searchValue).then((res) => {
+                console.log("search res", res.data);
+                setLoading(false);
+                setResults(
+                    res.data.map(
+                        (user: {
+                            id: number;
+                            username: string;
+                            displayname: string;
+                            pfp: string;
+                        }) => ({
+                            id: user.id,
+                            username: user.username,
+                            displayName: user.displayname,
+                            pfp: user.pfp,
+                        }),
+                    ),
+                );
+            });
+        }, 500),
+        [],
+    );
+
+    const handleSelect = (userId: number) => {
+        // navigate(`/community/${communityName}`);
+        console.log("invite user with id: ", userId);
+    };
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        const val = e.target.value;
+        setSearchValue(val);
+        if (val.length === 0) {
+            setResults([]);
+        } else {
+            setLoading(true);
+            getResults(val);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div
+                className={styles.searchWrapper}
+                onClick={(e) => e.stopPropagation()}
+            >
+                <img
+                    src={searchIcon}
+                    alt="search icon"
+                    className={styles.searchIcon}
+                />
+                <input
+                    placeholder="Search Users..."
+                    value={searchValue}
+                    onChange={handleChange}
+                    className={styles.searchInput}
+                />
+            </div>
+            <ul className={styles.communities}>
+                {loading ? (
+                    <LineSpinner width="24px" />
+                ) : results.length > 0 ? (
+                    results.map((user: UserInfo) => (
+                        <li
+                            key={user.id}
+                            className={styles.communityListItem}
+                            onClick={() => handleSelect(user.id!)}
+                        >
+                            <UserPreview
+                                communityId={communityId}
+                                profileUser={user}
+                                type="USER"
+                                role="Administrator"
+                            />
+                        </li>
+                    ))
+                ) : (
+                    <p className={styles.noCommunityMessage}>No match.</p>
+                )}
+            </ul>
+        </div>
+    );
+}
