beeee2157295048b558d7c5c68425d0483630385
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-01-26 14:42:10 +0400

    Fix: Modified the user profile components to show the skeletons when loading

--- client/src/components/UserProfilePage/UserLikes/UserLikes.tsx
+++ client/src/components/UserProfilePage/UserLikes/UserLikes.tsx
@@ -3,6 +3,7 @@ import {getLikesCurrentUser} from "../../../api/currentUser.ts";
 import Post from "../../PostStuff/Post/Post.tsx";
 import styles from "../UserContent.module.scss"
 import { useParams } from "react-router-dom";
+import UserPostsSkeleton from "../UserSkeletons/UserPostsSkeleton.tsx";
 
 export default function UserLikes () {
     const [likedPosts, setLikedPosts] = useState<React.ReactElement[]>([]);
@@ -12,56 +13,55 @@ export default function UserLikes () {
     useEffect(() => {
         if (!username) return;
         setIsLoading(true);
-        try {
-            getLikesCurrentUser().then((res) => {
-                if (res.data.length == 0) {
-                    setIsLoading(false);
-                    return;
+        getLikesCurrentUser().then((res) => {
+            if (res.data.length == 0) {
+                setIsLoading(false);
+                return;
+            }
+            const data = res.data;
+            const fetchedPosts: React.ReactElement[] = []
+            for (const post of data) {
+                const postInfo: PostInfo = {
+                    id: post.id,
+                    content: post.content,
+                    authorUsername: post.author,
+                    authorDisplayName: post.displayname,
+                    pfp: post.pfp,
+                    postDate: new Date(post.post_time),
+                    filename: post.attachment,
+                    likeCount: post.like_count,
+                    commentCount: post.comment_count,
+                    type: "POST",
+                    liked: post.liked,
+                };
+                const communityInfo: CommunityInfoSimple = {
+                    name: post.community,
+                    icon: post.community_icon
                 }
-                const data = res.data;
-                const fetchedPosts: React.ReactElement[] = []
-                for (const post of data) {
-                    const postInfo: PostInfo = {
-                        id: post.id,
-                        content: post.content,
-                        authorUsername: post.author,
-                        authorDisplayName: post.displayname,
-                        pfp: post.pfp,
-                        postDate: new Date(post.post_time),
-                        filename: post.attachment,
-                        likeCount: post.like_count,
-                        commentCount: post.comment_count,
-                        type: "POST",
-                        liked: post.liked,
-                    };
-                    const communityInfo: CommunityInfoSimple = {
-                        name: post.community,
-                        icon: post.community_icon
-                    }
-                    fetchedPosts.push(
-                        <Post
-                            key={post.id}
-                            postInfo={postInfo}
-                            communityInfo={communityInfo}
-                            from={`user/${username}`}
-                        />
-                    );
-                    setLikedPosts((prevPosts) => [...fetchedPosts, ...prevPosts]);
-                }
-            });
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
+                fetchedPosts.push(
+                    <Post
+                        key={post.id}
+                        postInfo={postInfo}
+                        communityInfo={communityInfo}
+                        from={`user/${username}`}
+                    />
+                );
+                setLikedPosts((prevPosts) => [...fetchedPosts, ...prevPosts]);
+            }
+        })
+            .catch((err) => {
+                console.error(err);
+            })
+            .finally(() => setIsLoading(false));
     }, [username]);
 
-    return (
+    return isLoading ? (
+        <UserPostsSkeleton />
+    ) : (
         <>
             <div className={styles.userContentContainer}>
                 {likedPosts}
             </div>
-            {isLoading && <span>Loading...</span>}
         </>
     )
 }
