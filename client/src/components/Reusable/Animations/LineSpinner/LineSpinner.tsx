e664249793c1405c1f2b9ac547912a221edf7f06
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-26 22:01:44 +0400

    fix: change spinner styles

--- client/src/components/Reusable/Animations/LineSpinner/LineSpinner.tsx
+++ client/src/components/Reusable/Animations/LineSpinner/LineSpinner.tsx
@@ -4,6 +4,7 @@ export default function LineSpinner({width}: {width: string}) {
     return (
         <div className={styles.container}>
             <div className={styles.loader} style={{width: width, height: width}}></div>
+            <div className={styles.inner} style={{width: width, height: width}}></div>
         </div>
     )
 }
