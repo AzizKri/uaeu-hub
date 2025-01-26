faa01d7736418e3090c4fba756df89c269956a5b
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-01-26 08:19:35 +0400

    fix: move all providers to a single provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-import UserProvider from './lib/providers/userProvider.tsx';
-import { UpdatePostProvider } from './lib/providers/UpdatePostProvider.tsx';
-import WebSocketProvider from './lib/providers/WebSocketProvider.tsx';
 import { Routes } from './routes.tsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import AppProviders from "./contexts/AppProviders.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <UserProvider>
-        <WebSocketProvider>
-            <UpdatePostProvider>
-                <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-                    <Routes />
-                </GoogleOAuthProvider>
-            </UpdatePostProvider>
-        </WebSocketProvider>
-    </UserProvider>
+    <AppProviders>
+        <Routes/>
+    </AppProviders>
 );
