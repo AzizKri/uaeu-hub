a350c2fcdd1659697a8481b4887e29fb6d58a078
AzizKri <aziz.kanakri@gmail.com>
2025-01-28 15:25:22 +0400

    Implement new Email Endpoints

--- server/src/v3/routes/auth.ts
+++ server/src/v3/routes/auth.ts
@@ -1,30 +1,33 @@
 import { Context, Hono } from 'hono';
 import { authMiddlewareCheckOnly } from '../middleware';
 import {
-    anonSignup,
     authenticateUser,
     authenticateWithGoogle,
+    forceLogout,
     isAnon,
     isUser,
     login,
-    logout,
-    signup
+    logout, sendEmailVerification,
+    signup, verifyEmail
 } from '../controllers/auth.controller';
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/me', authMiddlewareCheckOnly, (c) => authenticateUser(c));
-app.post('/signup', authMiddlewareCheckOnly, (c) => signup(c));
-app.post('/google', authMiddlewareCheckOnly, (c) => authenticateWithGoogle(c));
-app.post('/login', authMiddlewareCheckOnly, (c) => login(c));
-app.get('/logout', authMiddlewareCheckOnly, (c) => logout(c));
-app.get('/anon', authMiddlewareCheckOnly, (c) => anonSignup(c) as Promise<never>);
-app.get('/isUser', (c) => isUser(c));
-app.get('/isAnon', authMiddlewareCheckOnly, (c) => isAnon(c));
 // Login & Signup
 app.post('/signup', authMiddlewareCheckOnly, (c: Context) => signup(c));
 app.post('/google', authMiddlewareCheckOnly, (c: Context) => authenticateWithGoogle(c));
 app.post('/login', authMiddlewareCheckOnly, (c: Context) => login(c));
+app.get('/logout', authMiddlewareCheckOnly, (c: Context) => {
+    const force = c.req.query('force');
+    if (force) return forceLogout(c);
+    return logout(c);
+});
+// app.get('/anon', authMiddlewareCheckOnly, (c: Context) => anonSignup(c) as Promise<never>);
+
+// Email
+app.post('/sendEmailVerification', authMiddlewareCheckOnly, (c: Context) => sendEmailVerification(c));
+app.get('/verifyEmail', authMiddlewareCheckOnly, (c: Context) => verifyEmail(c));
+
 // User data
 
 app.get('/me', authMiddlewareCheckOnly, (c: Context) => authenticateUser(c));
