a3e7d61d789fb05c084e4e0283806a0823ac6391
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-01-28 17:45:42 +0400

    Made a reusable skeleton

---
+++ client/src/components/Reusable/Skeleton/Skeleton.tsx
@@ -0,0 +1,25 @@
+import styles from "./Skeleton.module.scss";
+
+type SkeletonProps = {
+    type: "post" | "community";
+};
+
+export default function Skeleton({ type }: SkeletonProps) {
+    if (type === "post") {
+        return (
+            <div className={styles.postSkeleton}>
+                <div className={styles.lineSkeleton} style={{ width: "30%" }} />
+                <div className={styles.lineSkeleton} style={{ width: "80%" }} />
+                <div className={styles.lineSkeleton} style={{ width: "90%" }} />
+            </div>
+        );
+    } else if (type === "community") {
+        return (
+            <div className={styles.communitySkeleton}>
+                <div className={styles.avatarSkeleton} />
+                <div className={styles.lineSkeleton} style={{ width: "60%" }} />
+            </div>
+        );
+    }
+    return null;
+}
