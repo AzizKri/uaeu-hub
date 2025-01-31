68c646e5581e9ca204b4422c973c96b9005aa9b3
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-01-31 19:52:04 +0400

    This component is now just the container and i added the tabs

--- client/src/components/UserProfilePage/EditUserPopUp/EditUserPopUp.tsx
+++ client/src/components/UserProfilePage/EditUserPopUp/EditUserPopUp.tsx
@@ -1,7 +1,16 @@
 import {useState} from 'react';
 import styles from './EditUserPopUp.module.scss';
 import Modal from "../../Reusable/Modal/Modal.tsx";
-import ImageUploader from "../../Reusable/ImageUploader/ImageUploader.tsx";
+import EditProfile from "./EditProfile.tsx";
+import ChangePassword from "./ChangePassword.tsx";
+
+type tab = "Edit Profile" | "Change Email" | "Change Password"
+
+const settingsTabs: {label: tab }[] = [
+    { label: "Edit Profile" },
+    { label: "Change Email" },
+    { label: "Change Password" },
+];
 
 interface EditUserPopUpProps {
     onClose: () => void;
@@ -18,51 +27,53 @@ export default function EditUserPopUp({
                                           currentBio,
                                           onSave
                                       }: EditUserPopUpProps) {
-    const [displayName, setDisplayName] = useState(currentDisplayName);
-    const [bio, setBio] = useState(currentBio);
-    const [uploadState, setUploadState] = useState<UploadState>({
-            status: "IDLE",
-            file: null,
-            preview: currentProfilePicture,
-            fileName: currentProfilePicture
-        }
-    );
+    // const [displayName, setDisplayName] = useState(currentDisplayName);
+    // const [bio, setBio] = useState(currentBio);
+    const [activeTab, setActiveTab] = useState<tab>("Edit Profile");
+    // const [uploadState, setUploadState] = useState<UploadState>({
+    //         status: "IDLE",
+    //         file: null,
+    //         preview: currentProfilePicture,
+    //         fileName: currentProfilePicture
+    //     }
+    // );
 
-    const handleSave = () => {
-        onSave(displayName, bio, (uploadState?.fileName ? uploadState.fileName : ''));
-        onClose();
+    // const handleSave = () => {
+    //     onSave(displayName, bio, (uploadState?.fileName ? uploadState.fileName : ''));
+    //     onClose();
+    // };
+
+    const handleTabClick = (tabLabel: tab) => {
+        setActiveTab(tabLabel);
     };
 
     return (
         <Modal onClose={onClose}>
             <div className={styles.container}>
-                <h2 className={styles.editHeader}>Edit Profile</h2>
-                <ImageUploader uploadState={uploadState} setUploadState={setUploadState} type="PROFILE"/>
-                <label htmlFor="displayName" className={styles.editLabel}>Display Name</label>
-                <input
-                    type="text"
-                    id="displayName"
-                    value={displayName}
-                    className={styles.editInput}
-                    onChange={(e) => setDisplayName(e.target.value)}
-                />
-
-                <label htmlFor="bio" className={styles.editLabel}>Bio</label>
-                <textarea
-                    id="bio"
-                    value={bio}
-                    className={styles.editTextArea}
-                    onChange={(e) => setBio(e.target.value)}
-                />
-
-                <div className={styles.buttons}>
-                    <button onClick={handleSave} className={styles.saveButton} disabled={uploadState.status === "UPLOADING"}>
-                        Save
-                    </button>
-                    <button onClick={onClose} className={styles.cancelButton} disabled={uploadState.status === "UPLOADING"}>
-                        Cancel
-                    </button>
-                </div>
+                <ul className={styles.tabs}>
+                    {settingsTabs.map((tab) => (
+                        <li
+                            key={tab.label}
+                            className={`${styles.tabElement} ${
+                                activeTab === tab.label
+                                    ? styles.active
+                                    : ""
+                            }`}
+                            onClick={() => handleTabClick(tab.label)}
+                        >
+                            {tab.label}
+                        </li>
+                    ))}
+                </ul>
+                {activeTab === "Edit Profile" ? (
+                    <EditProfile onSave={onSave} currentDisplayName={currentDisplayName} currentBio={currentBio} currentProfilePicture={currentProfilePicture} onClose={onClose}/>
+                ) : activeTab === "Change Email" ? (
+                    <></>
+                ) : activeTab === "Change Password" ? (
+                    <ChangePassword onClose={onClose}/>
+                ) : (
+                    <></>
+                )}
             </div>
         </Modal>
     );
