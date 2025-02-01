736873e7054c4802c6882a4a0fae5ad55f6599cb
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-02-01 19:30:33 +0400

    New signup using the reusable form

--- client/src/components/UserAccounts/SignUp/SignUp.tsx
+++ client/src/components/UserAccounts/SignUp/SignUp.tsx
@@ -6,9 +6,10 @@ import {isAnon} from '../../../api/currentUser.ts';
 import YesNoPopUp from "../../Reusable/YesNoPopUp/YesNoPopUp.tsx";
 import {userSchema} from "../../../userSchema.ts";
 import { z } from 'zod';
-import Requirement from "../Requirement/Requirement.tsx";
 import GoogleAuth from "../GoogleAuth/GoogleAuth.tsx";
 import {useUser} from "../../../contexts/user/UserContext.ts";
+import FormsContainer from "../../Reusable/Forms/FormsContainer.tsx";
+import FormItem from "../../Reusable/Forms/FormItem.tsx";
 
 export default function SignUp() {
     const navigate = useNavigate();
@@ -22,45 +23,14 @@ export default function SignUp() {
         includeAnon: true
     });
     const [errors, setErrors] = useState<SignUpErrors>({});
-    const [reqErrors, setReqErrors] = useState<requirementErrors>({
-        passLengthError: true,
-        passLowerError: true,
-        passUpperError: true,
-        passNumberError: true,
-        passSpecialError: true,
-    });
     const [showPopup, setShowPopup] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [passwordShown, setPasswordShown] = useState<boolean>(false);
-    const [isPasswordActive, setIsPasswordActive] = useState<boolean>(false);
     const location = useLocation();
     const previousPage = location.state?.from;
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { id, value } = e.target;
         setFormData({ ...formData, [id]: value });
-        if (id === "password") {
-            checkRequirements(value);
-        }
-    };
-
-    const handleChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { id, value } = e.target;
-        setFormData({ ...formData, [id]: value.toLowerCase() });
-    };
-
-    const checkRequirements = (password : string)=> {
-        const upperCasePattern = /[A-Z]/;
-        const lowerCasePattern = /[a-z]/;
-        const numberPattern = /\d/;
-        const specialPattern = /[^a-zA-Z0-9]/;
-        setReqErrors({
-            passLengthError: password.length < 8,
-            passLowerError: !lowerCasePattern.test(password),
-            passUpperError: !upperCasePattern.test(password),
-            passNumberError: !numberPattern.test(password),
-            passSpecialError: !specialPattern.test(password),
-        });
     };
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -122,7 +92,6 @@ export default function SignUp() {
     };
 
     const handlePopupResponse = async (choice: boolean) => {
-        // setShowPopup(false);
         setIsLoading(true);
         await processSignup(choice);
         setIsLoading(false);
@@ -131,18 +100,15 @@ export default function SignUp() {
     const handleHidePopUp = async () => {
         setShowPopup(false);
         setIsLoading(false);
-        // await processSignup(false);
     }
 
     const handleFocus = () => {
         setErrors({});
         setIsLoading(false);
-        setIsPasswordActive(false)
     };
 
     const handleFocusPassword = () => {
         handleFocus();
-        setIsPasswordActive(true);
     }
 
     const handleGoToLogin = () => {
@@ -182,137 +148,60 @@ export default function SignUp() {
                             {errors.global}
                         </p>
                     )}
-                    <form
-                        className={styles.form}
-                        onSubmit={handleSubmit}
-                        noValidate
+                    <FormsContainer>
+                        <FormItem
+                            type="text"
+                            id="username"
+                            label="Username"
+                            placeholder="Username"
+                            required={true}
+                            value={formData.username} // Pass value from parent state
+                            onChange={handleChange}    // Use your existing handler
+                            onFocus={handleFocus}
+                            error={errors.username}
+                        />
+                        <FormItem
+                            type="text"
+                            id="displayname"
+                            label="Display Name"
+                            placeholder="Display Name"
+                            value={formData.displayName}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            error={errors.displayName}
+                        />
+                        <FormItem
+                            type="text"
+                            id="email"
+                            label="Email"
+                            placeholder="Email"
+                            required={true}
+                            value={formData.email}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            error={errors.email}
+                        />
+                        <FormItem
+                            type="password"
+                            id="password"
+                            label="Password"
+                            placeholder="Password"
+                            required={true}
+                            togglePassword={true}
+                            showPasswordRequirements={true}
+                            value={formData.password}
+                            onChange={handleChange}
+                            onFocus={handleFocusPassword} // Activate password requirements display
+                            error={errors.password}
+                        />
+                    </FormsContainer>
+                    <button
+                        className={styles.formBtn}
+                        disabled={isLoading}
+                        onClick={handleSubmit}
                     >
-                        <div className={styles.formGroup}>
-                            <div className={styles.formGroup}>
-                                <label
-                                    htmlFor="username"
-                                    className={styles.formLabel}
-                                >
-                                    Username<span>*</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    className={`${styles.formInput} ${errors.username ? styles.invalidInput : ""}`}
-                                    id="username"
-                                    placeholder="Username"
-                                    value={formData.username}
-                                    onChange={handleChangeUsername}
-                                    onFocus={() => handleFocus()}
-                                />
-                                {errors.username && (
-                                    <small className={styles.error}>
-                                        {errors.username}
-                                    </small>
-                                )}
-                            </div>
-                            <label
-                                htmlFor="displayName"
-                                className={styles.formLabel}
-                            >
-                                Display Name
-                            </label>
-                            <input
-                                type="text"
-                                id="displayName"
-                                className={`${styles.formInput} ${errors.displayName ? styles.invalidInput : ""}`}
-                                placeholder="Display Name"
-                                value={formData.displayName}
-                                onChange={handleChange}
-                                onFocus={() => handleFocus()}
-                            />
-                            {errors.displayName && (
-                                <small className={styles.error}>
-                                    {errors.displayName}
-                                </small>
-                            )}
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label htmlFor="email" className={styles.formLabel}>
-                                Email<span>*</span>
-                            </label>
-                            <input
-                                type="text"
-                                className={`${styles.formInput} ${errors.email ? styles.invalidInput : ""}`}
-                                id="email"
-                                placeholder="Email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                onFocus={() => handleFocus()}
-                            />
-                            {errors.email && (
-                                <small className={styles.error}>
-                                    {errors.email}
-                                </small>
-                            )}
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label
-                                htmlFor="password"
-                                className={styles.formLabel}
-                            >
-                                Password<span>*</span>
-                            </label>
-                            <input
-                                type={passwordShown ? "text" : "password"}
-                                className={`${styles.formInput} ${errors.password ? styles.invalidInput : ""}`}
-                                id="password"
-                                placeholder="Password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                onFocus={() => handleFocusPassword()}
-                            />
-                            <span className={styles.showPassword} onClick={() => setPasswordShown((prev) => !prev)}>
-                                {passwordShown ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        width="24px"
-                                        height="24px"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z" />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        width="24px"
-                                        height="24px"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z" />
-                                    </svg>
-                                )}
-                            </span>
-                            {errors.password && (
-                                <small className={styles.error}>
-                                    {errors.password}
-                                </small>
-                            )}
-                            {isPasswordActive ? (
-                                <>
-                                <Requirement text={"Password must be at least 8 characters long"} error={reqErrors.passLengthError} />
-                                <Requirement text={"Password must contain at least one uppercase letter"} error={reqErrors.passUpperError} />
-                                <Requirement text={"Password must contain at least one lowercase letter"} error={reqErrors.passLowerError} />
-                                <Requirement text={"Password must contain at least one number"} error={reqErrors.passNumberError} />
-                                <Requirement text={"Password must contain at least one special character"} error={reqErrors.passSpecialError} />
-                                </>
-                                ) : (<span></span>
-                        )}
-                        </div>
-                        <button
-                            type="submit"
-                            className={styles.formBtn}
-                            disabled={isLoading}
-                        >
-                            {isLoading ? "Signing up..." : "Sign up"}
-                        </button>
-                    </form>
+                        {isLoading ? "Signing up..." : "Sign up"}
+                    </button>
                     <p className={styles.textParagraph}>
                         Already a member?{" "}
                         <span className={styles.formLink} onClick={handleGoToLogin}>
