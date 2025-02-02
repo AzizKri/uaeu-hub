5b972996cd9a28749f6b9255833fe0b8bc2d02c6
Mohammad <mohammadnaji126@gmail.com>
2025-02-02 23:37:59 +0400

    Added the necessary props for "change email" and "change password"

--- client/src/components/UserProfilePage/EditUserPopUp/EditUserPopUp.tsx
+++ client/src/components/UserProfilePage/EditUserPopUp/EditUserPopUp.tsx
@@ -3,6 +3,7 @@ import styles from './EditUserPopUp.module.scss';
 import Modal from "../../Reusable/Modal/Modal.tsx";
 import EditProfile from "../UserProfileSettings/EditProfile.tsx";
 import ChangePassword from "../UserProfileSettings/ChangePassword.tsx";
+import ChangeEmail from "../UserProfileSettings/ChangeEmail.tsx";
 
 type tab = "Edit Profile" | "Change Email" | "Change Password"
 
@@ -17,7 +18,10 @@ interface EditUserPopUpProps {
     currentProfilePicture?: string;
     currentDisplayName: string;
     currentBio: string;
-    onSave: (updatedDisplayName: string, updatedBio: string, updatedPfp: string) => void;
+    currentEmail: string;
+    onSaveEditProfile: (updatedDisplayName: string, updatedBio: string, updatedPfp: string) => void;
+    onSaveChangePassword: (currPass : string, newPass : string) => string | undefined;
+    isLoading: boolean;
 }
 
 export default function EditUserPopUp({
@@ -25,7 +29,10 @@ export default function EditUserPopUp({
                                           currentProfilePicture,
                                           currentDisplayName,
                                           currentBio,
-                                          onSave
+                                          currentEmail,
+                                          onSaveChangePassword,
+                                          onSaveEditProfile,
+                                          isLoading,
                                       }: EditUserPopUpProps) {
     // const [displayName, setDisplayName] = useState(currentDisplayName);
     // const [bio, setBio] = useState(currentBio);
@@ -66,11 +73,11 @@ export default function EditUserPopUp({
                     ))}
                 </ul>
                 {activeTab === "Edit Profile" ? (
-                    <EditProfile onSave={onSave} currentDisplayName={currentDisplayName} currentBio={currentBio} currentProfilePicture={currentProfilePicture} onClose={onClose}/>
+                    <EditProfile onSave={onSaveEditProfile} currentDisplayName={currentDisplayName} currentBio={currentBio} currentProfilePicture={currentProfilePicture} onClose={onClose} isLoading={isLoading} />
                 ) : activeTab === "Change Email" ? (
-                    <></>
+                    <ChangeEmail currentEmail={currentEmail} />
                 ) : activeTab === "Change Password" ? (
-                    <ChangePassword onClose={onClose}/>
+                    <ChangePassword onClose={onClose} onSave={onSaveChangePassword} isLoading={isLoading}/>
                 ) : (
                     <></>
                 )}
