34b0508f904cc6b47de5db1222561d490665caee
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-05-20 14:21:56 +0400

    remove unused dependencies and format

--- client/src/components/Communities/SearchUsers/SearchUsers.tsx
+++ client/src/components/Communities/SearchUsers/SearchUsers.tsx
@@ -1,7 +1,7 @@
 import styles from "./SearchUsers.module.scss";
 import React, { useCallback, useState } from "react";
 import { debounce } from "../../../utils/tools.ts";
-import {searchUsersWithStatusInCommunity} from "../../../api/users.ts";
+import { searchUsersWithStatusInCommunity } from "../../../api/users.ts";
 import searchIcon from "../../../assets/search.svg";
 import LineSpinner from "../../Reusable/Animations/LineSpinner/LineSpinner.tsx";
 import UserPreview from "../../UserPreview/UserPreview.tsx";
@@ -12,30 +12,35 @@ export default function SearchUsers({ communityId }: { communityId: number }) {
     const [loading, setLoading] = useState<boolean>(false);
 
     const getResults = useCallback(
-        debounce(async (searchValue: string) => {
-            searchUsersWithStatusInCommunity(searchValue, communityId).then((res) => {
-                console.log("search res", res.data);
-                setLoading(false);
-                setResults(
-                    res.data.map(
-                        (user: {
-                            id: number;
-                            username: string;
-                            displayname: string;
-                            pfp: string;
-                            status: 'member' | 'invited' | 'not_invited';
-                        }) => ({
-                            id: user.id,
-                            username: user.username,
-                            displayName: user.displayname,
-                            pfp: user.pfp,
-                            status: user.status,
-                        }),
-                    ),
+            debounce(async (searchValue: string) => {
+                searchUsersWithStatusInCommunity(searchValue, communityId).then(
+                    (res) => {
+                        console.log("search res", res.data);
+                        setLoading(false);
+                        setResults(
+                            res.data.map(
+                                (user: {
+                                    id: number;
+                                    username: string;
+                                    displayname: string;
+                                    pfp: string;
+                                    status:
+                                        | "member"
+                                        | "invited"
+                                        | "not_invited";
+                                }) => ({
+                                    id: user.id,
+                                    username: user.username,
+                                    displayName: user.displayname,
+                                    pfp: user.pfp,
+                                    status: user.status,
+                                }),
+                            ),
+                        );
+                    },
                 );
-            });
-        }, 500),
-        [],
+            }, 500),
+        [searchValue, communityId],
     );
 
     const handleSelect = (userId: number) => {
