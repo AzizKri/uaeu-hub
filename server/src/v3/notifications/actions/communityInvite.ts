27557b6e352348def6d72346bd0f9cae812ca233
AzizKri <aziz.kanakri@gmail.com>
2025-05-20 16:19:42 +0400

    Include communityName in notification metadata

--- server/src/v3/notifications/actions/communityInvite.ts
+++ server/src/v3/notifications/actions/communityInvite.ts
@@ -7,20 +7,30 @@ export async function handleCommunityInvite(env: Env, {
     communityId
 }: NotificationPayload.Invite) {
 
+    // Get community name
+    const community = await env.DB.prepare(`
+        SELECT name
+        FROM community
+        WHERE id = ?
+    `).bind(communityId).first<{ name: string }>();
+
+    const metadata = {
+        communityId: communityId,
+        communityName: community!.name
+    }
+
     // Insert notification into DB
     await env.DB.prepare(`
                 INSERT INTO notification (sender_id, recipient_id, type, action_entity_id, metadata)
                 VALUES (?, ?, 'invite', ?, ?)
-            `).bind(senderId, receiverId, inviteId, JSON.stringify({ communityId: communityId })).run();
+            `).bind(senderId, receiverId, inviteId, JSON.stringify(metadata)).run();
 
     const invitePayload: NotificationPayload.default = {
         senderId,
         receiverId,
         type: 'invite',
         actionEntityId: inviteId,
-        metadata: {
-            communityId: communityId
-        }
+        metadata: metadata
     };
     await sendToWebSocket(env, invitePayload);
 }
