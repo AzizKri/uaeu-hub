999d535691b9b07ce823942c5154979fdeb8c394
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-11-24 22:03:19 +0400

    Fixed images sometimes infinitely load

--- client/src/components/PostStuff/Content/Content.tsx
+++ client/src/components/PostStuff/Content/Content.tsx
@@ -1,67 +1,74 @@
 import styles from "./Content.module.scss";
 import React, { useState } from "react";
-import {assetsBase} from "../../../api/api.ts";
+import { assetsBase } from "../../../api/api.ts";
 import PostImage from "../../Reusable/PostImage/PostImage.tsx";
 // import { getAttachmentDetails } from "../../../api/attachmets.ts";
 
 export default function Content({
-    content,
-    filename,
-    type,
+  content,
+  filename,
+  type,
 }: {
-    content: string;
-    filename: string | undefined;
-    type: string;
+  content: string;
+  filename: string | undefined;
+  type: string;
 }) {
+  const [showContent, setShowContent] = useState<boolean>(content.length < 300);
+  // const [imageSrc, setImageSrc] = useState<string>("");
+  // const [imageDims, setImageDims] = useState<{
+  //     width: number;
+  //     height: number;
+  // }>({ width: 0, height: 0 });
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(filename != null);
 
-    const [showContent, setShowContent] = useState<boolean>(
-        content.length < 300,
-    );
-    // const [imageSrc, setImageSrc] = useState<string>("");
-    // const [imageDims, setImageDims] = useState<{
-    //     width: number;
-    //     height: number;
-    // }>({ width: 0, height: 0 });
-    const [error, setError] = useState<boolean>(false);
-    const [isLoading, setIsLoading] = useState<boolean>(filename != null);
+  const handleShowMore: React.MouseEventHandler<HTMLSpanElement> = (e) => {
+    e.stopPropagation();
+    setShowContent(true);
+  };
 
-    const handleShowMore: React.MouseEventHandler<HTMLSpanElement> = (e) => {
-        e.stopPropagation();
-        setShowContent(true);
-    };
+  const handleImageLoad = (event?: React.SyntheticEvent<HTMLImageElement>) => {
+    event?.stopPropagation();
+    if (!isLoading) return;
+    setIsLoading(false);
+  };
 
-    return (
-        <>
-            <div className={styles.text}>
-                {showContent || type === "post-page" ? (
-                    content
-                ) : (
-                    <>
-                        {content.slice(0, 200)}
-                        <span>&#8230;</span>
-                    </>
-                )}
-                {!showContent && !(type === "post-page") && (
-                    <span className={styles.show_more} onClick={handleShowMore}>
-                        show more
-                    </span>
-                )}
-            </div>
-            {/*<ReadOnlyEditor content={editorContent} />*/}
-            {/*{isLoading && !error && <LineSpinner spinnerRadius="100px" />}*/}
-            {filename && !error && (
-                <PostImage
-                    source={`${assetsBase}/attachments/${filename}`}
-                    alt="post attachment"
-                    onLoad={() => setIsLoading(false)}
-                    onError={() => {
-                        setError(true);
-                        setIsLoading(false);
-                    }}
-                    isLoading={isLoading}
-                    />
-            )}
-            {error && <p>Error Loading the image</p>}
-        </>
-    );
+  const handleImageError = (event?: React.SyntheticEvent<HTMLImageElement>) => {
+    event?.stopPropagation();
+    if (error) return;
+    setError(true);
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <div className={styles.text}>
+        {showContent || type === "post-page" ? (
+          content
+        ) : (
+          <>
+            {content.slice(0, 200)}
+            <span>&#8230;</span>
+          </>
+        )}
+        {!showContent && !(type === "post-page") && (
+          <span className={styles.show_more} onClick={handleShowMore}>
+            show more
+          </span>
+        )}
+      </div>
+      {/*<ReadOnlyEditor content={editorContent} />*/}
+      {/*{isLoading && !error && <LineSpinner spinnerRadius="100px" />}*/}
+      {filename && !error && (
+        <PostImage
+          source={`${assetsBase}/attachments/${filename}`}
+          alt="post attachment"
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+          isLoading={isLoading}
+        />
+      )}
+      {error && <p>Error Loading the image</p>}
+    </>
+  );
 }
